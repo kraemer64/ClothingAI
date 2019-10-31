332ea3991ddc24358cd4d065f22e806ad159887a
kraemer64 <54526557+kraemer64@users.noreply.github.com>
2019-10-31 23:12:59 +0100

    added shape

--- clothingAI.py
+++ clothingAI.py
@@ -16,3 +16,5 @@ fashion_mnist = keras.datasets.fashion_mnist
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+print(train_images.shape)
