83c1a66c96695fa642d1ee5432ef03eaf5733dfa
kraemer64 <54526557+kraemer64@users.noreply.github.com>
2019-10-31 20:46:09 +0100

    comms

--- clothingAI.py
+++ clothingAI.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 
 print(tf.__version__)
 
+# Loading train data
 fashion_mnist = keras.datasets.fashion_mnist
 
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
