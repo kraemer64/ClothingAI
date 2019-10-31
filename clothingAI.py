e7744f33e18302b246ec3f572babef4af6407b1d
kraemer64 <54526557+kraemer64@users.noreply.github.com>
2019-10-31 23:09:48 +0100

    adding class names

--- clothingAI.py
+++ clothingAI.py
@@ -14,3 +14,5 @@ print(tf.__version__)
 fashion_mnist = keras.datasets.fashion_mnist
 
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
