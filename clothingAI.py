0f9fbab5fccac3cb5edd934412ee1a1f5f6a68cc
kraemer64 <54526557+kraemer64@users.noreply.github.com>
2019-10-31 23:22:06 +0100

    plt preprocessing

--- clothingAI.py
+++ clothingAI.py
@@ -26,3 +26,17 @@ plt.imshow(train_images[0])
 plt.colorbar()
 plt.grid(False)
 plt.show()
+
+train_images = train_images / 255.0
+
+test_images = test_images / 255.0
+
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[train_labels[i]])
+plt.show()
