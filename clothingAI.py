9104d3dada1e719c6395c9ddcefdc96b195769bb
kraemer64 <54526557+kraemer64@users.noreply.github.com>
2020-03-28 16:50:52 +0100

    Deleted prints

--- clothingAI.py
+++ clothingAI.py
@@ -8,7 +8,6 @@ from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
 
-print(tf.__version__)
 
 # Loading train data
 fashion_mnist = keras.datasets.fashion_mnist
@@ -16,9 +15,6 @@ fashion_mnist = keras.datasets.fashion_mnist
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-
-print(train_images.shape)
-print(len(train_labels))
 
 # Preprocess clothing data
 plt.figure()
